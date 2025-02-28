0dbcac46d616fc5356478393d3b4fbb5216f6e0b
Anjalikava <kavaanjali2931@gmail.com>
2025-02-28 12:12:18 +0530

    done

--- src/screen/login-register/login/login.jsx
+++ src/screen/login-register/login/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 
 const Login = () => {
   const [input, setInput] = useState("");
